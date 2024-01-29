baaf4e84606b7c0e2a58b247e7e1a22d7b435fbb
Liana Mazayan <mazayanlian@gmail.com>
2024-01-29 23:38:03 +0400

    Add explaining comment.

--- EmployeeManagementSystem/Program.cs
+++ EmployeeManagementSystem/Program.cs
@@ -4,6 +4,7 @@ namespace EmployeeManagementSystem
 {
     class Program
     {
+        // Assuming you first compiled the Employee project and got the dll into Employee\bin\Debug\net6.0\Employee.dll
         static void Main()
         {
             Validator.DisplayTypeInformation(typeof(Employee));
