8a7eccdaefdff759dcf270cd036059b5d414ef83
Liana Mazayan <mazayanlian@gmail.com>
2024-01-29 23:31:14 +0400

    Add Employee Class Library

---
+++ Employee/Employee.cs
@@ -0,0 +1,25 @@
+﻿namespace EmployeeLibrary
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        [SalaryValidation(1000, 100000)]
+        public double Salary { get; set; }
+
+        public Employee(int employeeId, string name, double salary)
+        {
+            Id = employeeId;
+            Name = name;
+            Salary = salary;
+        }
+
+        public void DisplayInfo()
+        {
+            Console.WriteLine($"Employee ID: {Id}");
+            Console.WriteLine($"Name: {Name}");
+            Console.WriteLine($"Salary: ${Salary}");
+        }
+    }
+}
---
+++ Employee/SalaryValidationAttribute.cs
@@ -0,0 +1,15 @@
+﻿namespace EmployeeLibrary
+{
+    [AttributeUsage(AttributeTargets.Property, AllowMultiple = false)]
+    public class SalaryValidationAttribute : Attribute
+    {
+        public double MinSalary { get; }
+        public double MaxSalary { get; }
+
+        public SalaryValidationAttribute(double minSalary, double maxSalary)
+        {
+            MinSalary = minSalary;
+            MaxSalary = maxSalary;
+        }
+    }
+}
